00a0f7911fa00b7de1afef043a0eb4bad65b40f8
leejihoon <ljhljh211@naver.com>
2023-09-17 20:57:05 +0900

    fix: 라우팅 이동시에 idx 맞게 추가되도록 수정

--- src/pages/food/detail.ts
+++ src/pages/food/detail.ts
@@ -7,11 +7,24 @@ import foodOption from "@/components/food/foodOption";
 import foodPrice from "@/components/food/foodPrice";
 import { selectedFoodInfo } from "@/apis/food/types";
 
-let isEventListenerAdded = false;
+// let isEventListenerAdded = false;
 
-export default async function foodDetailPage(target: Element, params?: Params) {
+/* 선택된 상품, 옵션 정보 */
+let selectedMenuInfo = (idx: string): selectedFoodInfo => {
+  return {
+    foodIdx: idx,
+    optionIdx: [],
+    quantity: 0,
+  };
+};
+
+export default async function foodDetailPage(
+  cTarget: Element,
+  params?: Params
+) {
   const idx = params?.["foodIdx"]!;
   console.log("idx", idx);
+  const currentInfo = selectedMenuInfo(idx);
   let template = `{{__header__}}
   {{__food_info__}}
 
@@ -23,13 +36,6 @@ export default async function foodDetailPage(target: Element, params?: Params) {
   `;
   let selectedPrice = 0;
   let totalPrice = 0;
-
-  /* 선택된 상품, 옵션 정보 */
-  let selectedMenuInfo: selectedFoodInfo = {
-    foodIdx: idx,
-    optionIdx: [],
-    quantity: 0,
-  };
 
   try {
     const headerElement = header({ title: "음식 상세", hasBack: true });
@@ -56,79 +62,99 @@ export default async function foodDetailPage(target: Element, params?: Params) {
     const bottomSheet = foodPrice({ price: totalPrice });
     template = template.replace("{{__bottom_sheet__}}", bottomSheet);
 
-    target.innerHTML = template;
+    cTarget.innerHTML = template;
   } catch {
-    target.innerHTML = `<p>데이터 없음</p>`;
+    cTarget.innerHTML = `<p>데이터 없음</p>`;
   }
 
   /* 
   TODO
   - 이벤트 관리, 유틸함수로 관리할 수 있을지 확인
   */
-  if (!isEventListenerAdded) {
-    window.addEventListener("change", (event) => {
-      const target = event.target as HTMLInputElement;
-      const targetId = target?.getAttribute("id") ?? "";
+  function changeEventHandler(event: Event) {
+    if (!document.contains(cTarget)) {
+      cTarget.removeEventListener("change", changeEventHandler);
+      return;
+    }
+    const target = event.target as HTMLInputElement;
+    const targetId = target?.getAttribute("id") ?? "";
+    // const currentInfo = selectedMenuInfo(idx);
 
-      console.log("selectedMenuInfo", selectedMenuInfo);
-      if (target.closest("#price-option") && target.type === "checkbox") {
-        if (target.checked) {
-          selectedPrice += Number(target?.value);
+    console.log("selectedMenuInfo", currentInfo);
+    if (target.closest("#price-option") && target.type === "checkbox") {
+      if (target.checked) {
+        selectedPrice += Number(target?.value);
 
-          selectedMenuInfo.optionIdx = [
-            ...selectedMenuInfo.optionIdx,
-            targetId,
-          ];
-        } else {
-          selectedPrice -= Number(target?.value); // 체크가 해제된 경우 값을 감소
+        currentInfo.optionIdx = [...currentInfo.optionIdx, targetId];
+      } else {
+        selectedPrice -= Number(target?.value); // 체크가 해제된 경우 값을 감소
 
-          // id 값을 배열에서 제거
-          const index = selectedMenuInfo.optionIdx.indexOf(targetId);
-          if (index > -1) {
-            selectedMenuInfo.optionIdx.splice(index, 1);
-          }
-        }
-        // 가격 정보 업데이트
-        const totalPriceElement = document.getElementById("total-price");
-        if (totalPriceElement) {
-          totalPriceElement.textContent =
-            (totalPrice + selectedPrice).toLocaleString() + "원";
+        // id 값을 배열에서 제거
+        const index = currentInfo.optionIdx.indexOf(targetId);
+        if (index > -1) {
+          currentInfo.optionIdx.splice(index, 1);
         }
       }
-    });
-    window.addEventListener("click", (event) => {
-      const target = event.target as HTMLElement;
-      if (target.closest("#btn-add-cart")) {
-        alert("장바구니에 담았습니다.");
-
-        addToCart({
-          ...selectedMenuInfo,
-          foodIdx: idx,
-        });
+      // 가격 정보 업데이트
+      const totalPriceElement = document.getElementById("total-price");
+      if (totalPriceElement) {
+        totalPriceElement.textContent =
+          (totalPrice + selectedPrice).toLocaleString() + "원";
       }
-
-      // 장바구니 추가
-      function addToCart(selectedMenuInfo: selectedFoodInfo) {
-        // 로컬 스토리지에서 기존 장바구니 데이터
-        let cart: selectedFoodInfo[] =
-          JSON.parse(localStorage.getItem("cart") as string) || [];
-
-        const existingItem = cart.find(
-          (item: selectedFoodInfo) => item.foodIdx === selectedMenuInfo.foodIdx
-        );
-        if (
-          existingItem &&
-          existingItem.optionIdx.join("") ===
-            selectedMenuInfo.optionIdx.join("")
-        ) {
-          existingItem.quantity += 1; // 수량 증가
-        } else {
-          selectedMenuInfo.quantity = 1; // 새로운 아이템이라면 수량을 1로 설정
-          cart.push(selectedMenuInfo);
-        }
-        localStorage.setItem("cart", JSON.stringify(cart));
-      }
-    });
-    isEventListenerAdded = true;
+    }
   }
+
+  function clickEventHandler(event: Event) {
+    if(!document.contains(cTarget)) {
+      cTarget.removeEventListener("click", clickEventHandler);
+      return;
+  }
+    const target = event.target as HTMLElement;
+    // const currentInfo = selectedMenuInfo(idx);
+
+    if (target.closest("#btn-add-cart")) {
+      // alert("장바구니에 담았습니다.");
+      console.log("selectedMenuInfo", selectedMenuInfo);
+      console.log("idx", idx);
+
+      addToCart({
+        ...currentInfo,
+        foodIdx: idx,
+      });
+    }
+
+    // 장바구니 추가
+    function addToCart(selectedMenuInfo: selectedFoodInfo) {
+      // 로컬 스토리지에서 기존 장바구니 데이터
+      let cart: selectedFoodInfo[] =
+        JSON.parse(localStorage.getItem("cart") as string) || [];
+
+      const existingItem = cart.find(
+        (item: selectedFoodInfo) => item.foodIdx === selectedMenuInfo.foodIdx
+      );
+      if (
+        existingItem &&
+        existingItem.optionIdx.join("") === selectedMenuInfo.optionIdx.join("")
+      ) {
+        existingItem.quantity += 1; // 수량 증가
+      } else {
+        selectedMenuInfo.quantity = 1; // 새로운 아이템이라면 수량을 1로 설정
+        cart.push(selectedMenuInfo);
+      }
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  }
+  // if (!isEventListenerAdded) {
+  //   window.addEventListener("change", changeEventHandler);
+  //   window.addEventListener("click", clickEventHandler);
+  //   isEventListenerAdded = true;
+  // }
+
+  // 이전에 등록한 이벤트 리스너가 있다면 제거
+  cTarget.removeEventListener("change", changeEventHandler);
+  cTarget.removeEventListener("click", clickEventHandler);
+
+  // 새로운 이벤트 리스너 등록
+  cTarget.addEventListener("change", changeEventHandler);
+  cTarget.addEventListener("click", clickEventHandler);
 }
