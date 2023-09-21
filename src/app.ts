81b041447b0388ba58614ea0c4bfdfe56970b570
leejihoon <ljhljh211@naver.com>
2023-09-21 22:19:03 +0900

    comment: 코멘트 수정

--- src/app.ts
+++ src/app.ts
@@ -9,16 +9,12 @@ import FoodDetailPage from "./pages/food/detail";
 import CartPage from "./pages/cart/cart";
 import NotFoundPage from "./pages/404";
 
-// const pages = createPages("app");
-
 /* 
 라우트 설정
-경로 - 경로에 맞는 함수 호출되도록 설정
-일치하는 경로 없을 경우 page.notFound 호출
-init() 메서드로 라우터 시작
-- 초기 경로 확인, 페이지 내에서 라우트 변경을 감지하기 위한 리스너 설정
+- url경로-페이지 인스턴스를 전달
+- 일치하는 경로 없을 경우 notFoundPage 페이지 인스턴스 전달
+- init() 메서드로 라우터 시작
 */
-// const router = createRouter();
 const router = new Router();
 const homePage = new HomePage("app");
 const storeDetailPage = new StoreDetailPage("app");
